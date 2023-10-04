28023bc175af7d03da7e699fc6d999cfaa5ef0d2
Guenther Beulen <guenther.beulen@web.de>
2023-10-04 11:55:19 +0200

    Initial commit.

---
+++ src/rgb_led_control/interface_serial.h
@@ -0,0 +1,20 @@
+/**
+  * @file
+  * @brief interfaces eeprom content to play_of_lights
+  */
+
+#ifndef _INTERFACE_EEPROM_H
+#define _INTERFACE_EEPROM_H
+
+#include stdint.h
+#include Led.h
+
+const uint8_t index_number_of_leds;
+
+class EepromContent
+{
+
+};
+
+
+#endif //_INTERFACE_EEPROM_Huint8_
